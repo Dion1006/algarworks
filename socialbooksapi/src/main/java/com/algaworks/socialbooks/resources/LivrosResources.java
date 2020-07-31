8a69fa6b6bb6788a5c9cf1ceef7da5719faf7833
dion <dion.carlos@gmail.com>
2020-07-30 23:17:07 -0300

    curso Spring boot

---
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -0,0 +1,52 @@
+package com.algaworks.socialbooks.resources;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.socialbooks.domain.Livro;
+import com.algaworks.socialbooks.repository.LivrosRepository;
+
+@RestController
+@RequestMapping("/livros")
+public class LivrosResources {
+
+	@Autowired
+	private LivrosRepository livrosRepository;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Livro> Listar() {
+		return livrosRepository.findAll();
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		livrosRepository.save(livro);
+
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Optional<Livro> buscar(@PathVariable("id") Long id) {
+		return livrosRepository.findById(id);
+
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void deletar(@PathVariable("id") Long id) {
+		livrosRepository.deleteById(id);
+
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public void atualizar(@RequestBody Livro livro, @PathVariable("id") Long id) {
+			livro.setId(id);
+			livrosRepository.save(livro);
+	}
+
+}
